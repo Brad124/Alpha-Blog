c92ad30507db52472a6a36662b7e63e6065c5a3b
brad124 <bradesnyder12@gmail.com>
2016-02-01 08:44:52 +0000

    2nd try

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,20 +8,23 @@ class ArticlesController < ApplicationController
     
     
     def create
-        
         #render plain: params[:article].inspect
+        
         @article = Article.new(article_params)
         @article.save
         redirect_to articles_show(@article)
-    
+        
         
     end
     
     private
-      def article_params
-          params.require(:article).permit(:title, :description)
-          
-      end
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+        
+      
+     
 
-
+    
+    
 end

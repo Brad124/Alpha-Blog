40f32cb7195ef1eefcdba402d3d16b8bc6ea055e
brad124 <bradesnyder12@gmail.com>
2016-02-03 00:44:07 +0000

    partials destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -58,6 +58,20 @@ class ArticlesController < ApplicationController
        @article = Article.find(params[:id])
     end
     
+    
+    def destroy
+       @article = Article.find(params[:id])
+       @article.destroy
+       flash[:notice] = "Article was fucking deleted"
+       
+       redirect_to articles_path
+    
+    
+    
+    
+    
+    end
+    
     private
     def article_params
         params.require(:article).permit(:title, :description)

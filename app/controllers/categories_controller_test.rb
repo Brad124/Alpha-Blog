4aca34ba9f88ae36363ce1a29b6aa6a65058fc60
brad124 <bradesnyder12@gmail.com>
2016-02-17 08:01:42 +0000

    navbar

--- app/controllers/categories_controller_test.rb
+++ app/controllers/categories_controller_test.rb
@@ -4,7 +4,7 @@ class CategoriesControllerTest < ActionController::TestCase
     
     def setup
         @category = Category.create(name: "sports")
-        @user = User.create(username: "john", email: "john@fu.com", password: "pass", admin: true)
+        @user = User.create(username: "john", email: "john@fu.com", password: "password", admin: true)
     end
     
     test "should get categories index" do
